1288b9b8b36a9d44abd28e5af6a3e740fd7c9174
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-08-17 21:25:29 -0500

    First set of urls.

--- oldmail/urls.py
+++ oldmail/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^', include('gmail.urls')),
 )
