a93c480146c4fa8a0f6927970862258a8acf9aca
Jenny Qian <jqian@schipul.com>
2012-08-19 17:50:11 -0500

    Remove the unused code.

--- oldmail/urls.py
+++ oldmail/urls.py
@@ -11,12 +11,6 @@ urlpatterns = patterns('oldmail.views',
 
     url(r'^oauth1/$', 'oauth_connect', name="oauth_connect"),
 
-    url(r'^oauth2/$', 'oauth2', name="oauth2"),
-    url(r'^oauth2callback/$', 'oauth2_callback', name="oauth2_callback"),
-
-    url(r'^oauth/$', 'get_request_token', name="get_request_token"),
-    url(r'^oauthcallback/$', 'oauth_callback', name="oauth_callback"),
-
     url(r'^profile/activate/(?P<random_string>[\w]+)/$', views.ProfileAddView.as_view(), name="profile_activate"),
     url(r'^profile/verify/(?P<random_string>[\w]+)/$', views.ProfileVerifyView.as_view(), name="profile_verify"),
     url(r'^profile/list/$', views.ProfileList.as_view(), name="profile_list"),
