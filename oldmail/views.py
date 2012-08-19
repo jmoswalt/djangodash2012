7f8fb939b3a2443d383b3926dc37f159387c666a
jmoswalt <jmoswalt@gmail.com>
2012-08-19 01:33:53 -0500

    Moved authentication views to the top of the list.

--- oldmail/views.py
+++ oldmail/views.py
@@ -23,6 +23,69 @@ from oldmail.decorators import staff_or_super_required
 from oldmail.utils import send_email, random_string
 
 
+#@login_required
+def authenticate(request, slug, template_name='authenticate.html'):
+    """
+    Authenticate a user with his/her gmail account.
+    The user can grant or denied the access.
+
+    access_type: online or offline
+    approval_prompt: force or auto
+    """
+    # construct the url to authenticate
+    if not all([hasattr(settings, 'OAUTH2_CLIENT_ID'),
+                hasattr(settings, 'OAUTH2_REDIRECT_URL')]):
+        raise Http404
+
+    if request.method == "POST":
+        url = settings.OAUTH2_ENDPOINT
+        params = {'scope': settings.OAUTH2_SCOPE,
+                  'client_id': settings.OAUTH2_CLIENT_ID,
+                  'redirect_uri': settings.OAUTH2_REDIRECT_URL,
+                  'response_type': 'code',
+                  'state': '',
+                  'access_type': 'offline',
+                  'approval_prompt': 'auto'}
+        #TODO: assign user to the state param
+        url = '%s?%s' % (url, urllib.urlencode(params))
+
+        return HttpResponseRedirect(url)
+
+    return render_to_response(template_name, {'folder_name': request.user.profile.sync_label},
+            context_instance=RequestContext(request))
+
+
+def authenticate_callback(request):
+    """
+    Handle the response after the auth request gets sent back to the site.
+    """
+    # first, get the authorization code.
+    error = request.GET.get('error', '')
+    if error == 'access_denied':
+        # redirect to let them try again
+        messages.add_message(request, messages.ERROR, 'Access denied.')
+        return HttpResponseRedirect(reverse('authenticate'))
+
+    code = request.GET.get('code', '')
+    state = request.GET.get('state', '')
+
+    # exachange the authorization code for an access token and a refresh token
+    params = {'code': code,
+              'client_id': settings.OAUTH2_CLIENT_ID,
+              'client_secret': settings.OAUTH2_CLIENT_SECRET,
+              'redirect_uri': settings.OAUTH2_REDIRECT_URL,
+              'grant_type': 'authorization_code'}
+    result = urllib2.urlopen('http://example.com', urllib.urlencode(params))
+    content = result.read()
+
+    # a successful response is returned as a JSON array
+    content_d = simplejson.loads(content)
+    access_token = content_d['access_token']
+    expires_in = content_d['expires_in']
+    token_type = content_d['token_type']
+    refresh_token = content_d['refresh_token']
+
+
 class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
@@ -255,70 +318,6 @@ class ClientMessageList(ListView):
         qs.order_by('m_date')
 
         return qs
-
-
-#@login_required
-def authenticate(request, template_name = 'authenticate.html'):
-    """
-    Authenticate a user with his/her gmail account. 
-    The user can grant or denied the access.
-    
-    access_type: online or offline
-    approval_prompt: force or auto
-    """
-    # construct the url to authenticate
-    if not all([hasattr(settings, 'OAUTH2_CLIENT_ID'),
-                hasattr(settings, 'OAUTH2_REDIRECT_URL')]):
-        raise Http404
-    
-    if request.method == "POST":
-        url = settings.OAUTH2_ENDPOINT
-        params = {'scope': settings.OAUTH2_SCOPE,
-                  'client_id': settings.OAUTH2_CLIENT_ID,
-                  'redirect_uri': settings.OAUTH2_REDIRECT_URL,
-                  'response_type': 'code',
-                  'state': '',
-                  'access_type': 'offline',
-                  'approval_prompt': 'auto'} 
-        #TODO: assign user to the state param
-        url = '%s?%s' % (url, urllib.urlencode(params))
-        
-        return HttpResponseRedirect(url)
-    
-    return render_to_response(template_name, {'folder_name': 
-                                       settings.CLIENT_FOLDER_NAME},
-            context_instance=RequestContext(request))
-
-
-def authenticate_callback(request):
-    """
-    Handle the response after the auth request gets sent back to the site.
-    """
-    # first, get the authorization code.
-    error = request.GET.get('error', '')
-    if error == 'access_denied':
-        # redirect to let them try again
-        messages.add_message(request, messages.ERROR, 'Access denied.')
-        return HttpResponseRedirect(reverse('authenticate'))
-
-    code = request.GET.get('code', '')
-    state = request.GET.get('state', '')
-
-    # exachange the authorization code for an access token and a refresh token
-    params = {'code': code,
-              'client_id': settings.OAUTH2_CLIENT_ID,
-              'client_secret': settings.OAUTH2_CLIENT_SECRET,
-              'redirect_uri': settings.OAUTH2_REDIRECT_URL,
-              'grant_type': 'authorization_code'}
-    result = urllib2.urlopen('http://example.com', urllib.urlencode(params))
-    content = result.read()
-
-    # a successful response is returned as a JSON array
-    content_d = simplejson.loads(content)
-    access_token = content_d['access_token']
-    expires_in = content_d['expires_in']
-    token_type = content_d['token_type']
-    refresh_token = content_d['refresh_token']
 
 
 class ClientDetail(LoginRequiredMixin, DetailView):
